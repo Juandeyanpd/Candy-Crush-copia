8bb8a3078ea7ebe612b9d863ac2f719fbf2eea37
mfpelaez <mfpelaez13@misena.edu.co>
2022-09-21 13:47:52 -0500

    ya

--- Candy/Assets/Scripth/Score.cs
+++ Candy/Assets/Scripth/Score.cs
@@ -2,15 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
     public int score = 0;
-    public Text scorre;
+    public TMP_Text scorre;
 
     public void Scorre()
     {
-        scorre.text = "Score: " + score;
+        scorre.text = score.ToString("Score: ") + score;
+        
     }
 
     public void Puntaje(int cantidad)
